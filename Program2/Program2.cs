b2f619024c047f3153c2a3d34b5ec096a3811119
상해꽃 이 <mxj1234@naver.com>
2023-04-14 21:08:27 +0900

    feat: 2번문제 구현 문장을 Split 함수를 이용하여 나눈후 배열로 저장한후 배열의 길이를 재서 단어갯수 확인.

---
+++ Program2/Program2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest.Program2
+{
+    internal class Program2
+    {
+
+        public void Main2()
+        {
+            Console.Write("문장을 입력하시오. :");
+            string sentence = Console.ReadLine();
+            string[] sentenceLength = sentence.Split(' ');
+            Console.WriteLine(sentenceLength.Length);
+
+        }
+    }
+}
