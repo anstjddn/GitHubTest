d2ae514b9b74e0dd833db6b69d2ddfd0cbe1f80e
상해꽃 이 <mxj1234@naver.com>
2023-04-14 21:12:03 +0900

    feat : 3번문제 구현 입력한 숫자를 int형으로 반환후 for구문을 이용하여 소수판단

---
+++ Program3/Program3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest.Program3
+{
+    internal class Program3
+    {
+        public void Main3()
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num == 2) { Console.WriteLine("{0}은 소수입니다.", num); }
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("{0}은 소수가 아닙니다.", num);
+                }
+                else Console.WriteLine("{0}은 소수입니다.", num);
+            }
+        }
+    }
+}
