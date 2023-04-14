89fefa74f65b42f7ffc78a5cee0aa929989fa964
상해꽃 이 <mxj1234@naver.com>
2023-04-14 21:05:16 +0900

    feat : 1번 문제 구현 indexof 함수를 이용하여 문장에서 내가 입력한 단어의 위치를 출력

---
+++ Program1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest.Program1
+{
+    internal class Class1
+    {
+        public void Main1()
+        {
+            Console.Write("1. 원하는 문장을 입력하시오: ");
+            string sentence = Console.ReadLine();
+            Console.Write("찾고싶은 문장을 입력하시오:");
+            string str = Console.ReadLine();
+            Console.WriteLine(sentence.IndexOf(str));
+        }
+        
+    }
+}
