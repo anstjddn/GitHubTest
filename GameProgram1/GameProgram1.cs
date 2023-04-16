8215a742bb0ad626684c392830dddf2cb298e64d
상해꽃 이 <mxj1234@naver.com>
2023-04-17 03:19:41 +0900

    feat: 게임 1번 구현 랜덤클래스를 이용하여 랜덤한 수를 배정받고 while문을 이용하여 숫자비교를 한후 10회를 사용하면 if문으로 다시 횟수를 채우는방식으로 구현

---
+++ GameProgram1/GameProgram1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest.GameProgram1
+{
+    internal class GameProgram1
+    {
+        public void GameMain1()
+        {
+            int Chance = 10;
+            Random random = new Random();
+            int RandomNum = random.Next(1000);
+            Console.WriteLine(RandomNum);
+
+
+            while (Chance > 0)
+            {
+                Console.WriteLine("숫자를 입력하시오:");
+                int userNum = int.Parse(Console.ReadLine());
+                if (userNum < RandomNum)
+                {
+                    Console.WriteLine("더 크다.");
+                }
+                if (userNum > RandomNum)
+                {
+                    Console.WriteLine("더 작다");
+
+                }
+                if (userNum == RandomNum)
+                {
+                    Console.WriteLine("정답");
+                    break;
+                }
+                Chance--;
+                Console.WriteLine(Chance);
+                if (Chance == 0)
+                {
+                    Console.WriteLine("다시하실꺼면 재시작을 입력하시오");
+                    string user = Console.ReadLine();
+                    if (user == "재시작")
+                    {
+                        Chance = 10;
+                    }
+                }
+            }
+        }
+    }
+}
