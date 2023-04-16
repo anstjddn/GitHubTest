f726623801ac771312d0ff2f0feaf5477471613e
상해꽃 이 <mxj1234@naver.com>
2023-04-17 02:54:53 +0900

    feat : 4번문제 구현 10이하일떄랑 10이상일떄랑 나누고 10으로 나눠서 소수점을 계속 더하는 방식으로 자리수합구현

---
+++ Program4/Program4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTest.Program4
+{
+    internal class Program4
+    {
+        public void Main4()
+        {
+            int Num2 = 0;
+            Console.WriteLine("숫자를 입력하시오:");
+            int Num = int.Parse(Console.ReadLine());
+            if (Num < 10)
+            {
+                Console.Write(Num);
+            }
+
+            if (Num > 10)
+            {
+                while (Num > 0)
+                {
+                    int Num1 = Num % 10;
+                    Num = Num / 10;
+                    Num2 += Num1;
+                }
+                Console.WriteLine("자리수합은 :{0}", Num2);
+            }
+        }
+    }
+}
